f06456a906286f411b04299684d7c2bd09f4aaa3
JEONG-J <ikejhc159@gmail.com>
2025-01-08 22:32:38 +0900

    📝 Git 파일 추가
    풀리퀘 파일 및 이슈 템플릿 추가, 프로젝트 파일 추가

---
+++ Catchy/Catchy/CatchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchyApp.swift
+//  Catchy
+//
+//  Created by 정의찬 on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
