6711963a557275aa4696e1d798090fc5835d8525
JEONG-J <ikejhc159@gmail.com>
2025-01-13 18:00:12 +0900

    ✨ Shadow Reuse 작성
    Shadow Reuse 생성

--- Catchy/Catchy/Resources/Shadow/ShadowReuse.swift
+++ Catchy/Catchy/Resources/Shadow/ShadowReuse.swift
@@ -6,4 +6,50 @@
 //
 
 import Foundation
+import SwiftUI
 
+struct S1W: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 0)
+    }
+}
+
+struct S2T: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: .black.opacity(0.6), radius: 3, x: 0, y: 0)
+    }
+}
+
+struct S3I: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: .black.opacity(0.3), radius: 2.5, x: 0, y: 0)
+    }
+}
+
+struct S4B: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: .black.opacity(0.2), radius: 2, x: 0, y: 0)
+    }
+}
+
+extension View {
+    func s1w() -> some View {
+        self.modifier(S1W())
+    }
+    
+    func s2t() -> some View {
+        self.modifier(S2T())
+    }
+    
+    func s3i() -> some View {
+        self.modifier(S3I())
+    }
+    
+    func s4b() -> some View {
+        self.modifier(S4B())
+    }
+}
