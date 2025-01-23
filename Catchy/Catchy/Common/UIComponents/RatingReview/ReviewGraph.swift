1025bfe3766c8911ed0893889a532eb6b6bd5481
yongbin <chosh0312@naver.com>
2025-01-23 15:56:06 +0900

    💡 리뷰 평점 뷰 주석 추가
    리뷰 평점 뷰 주석 추가

--- Catchy/Catchy/Common/UIComponents/RatingReview/ReviewGraph.swift
+++ Catchy/Catchy/Common/UIComponents/RatingReview/ReviewGraph.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct ReviewGraph: View {
     
+    /// 점수(1~5점), 해당 점수를 준 사람 수, 전체 리뷰 작성자 수
     let score: Int
     let personCount: Int
     let totalPersonCount: Int
     
+    /// 초기화 메서드
+    /// - Parameters:
+    ///   - score: 점수 값
+    ///   - personCount: 해당 점수를 준 사람 수
+    ///   - totalPersonCount: 전체 리뷰 작성자 수
     init(score: Int, personCount: Int, totalPersonCount: Int) {
         self.score = score
         self.personCount = personCount
@@ -21,11 +27,13 @@ struct ReviewGraph: View {
     
     var body: some View {
         HStack(alignment: .center, spacing: 10, content: {
+            /// 점수 텍스트 표시
             Text("\(score)점")
                 .frame(width: 13, alignment: .leading)
                 .font(.caption2)
                 .foregroundColor(.g5)
             
+            /// 진행 상태 표시 바 (ProgressView)
             ProgressView(value: Double(personCount) / Double(totalPersonCount), total: 1.0)
                 .progressViewStyle(.linear)
                 .background(Color.g2)
@@ -35,7 +43,7 @@ struct ReviewGraph: View {
                 .clipShape(RoundedRectangle(cornerRadius: 5.5))
                 
 
-            
+            /// 특정 점수를 준 사람 수 표시 (최대 999명으로 제한)
             if personCount <= 999 {
                 Text("\(personCount)명")
                     .font(.caption2)
