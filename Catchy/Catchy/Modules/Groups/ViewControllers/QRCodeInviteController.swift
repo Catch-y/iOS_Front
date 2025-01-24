5bd5e5e48563060f87adffc607e06a9903d67789
soeunny11 <dlathdms1206@gmail.com>
2025-01-25 01:58:22 +0900

    :lipstick: 초대 QR코드 페이지 디자인 추가
    QR코드 생성 이미지 생길때 가운데 이미지가 뜨도록 수정했습니다

--- Catchy/Catchy/Modules/Groups/ViewControllers/QRCodeInviteController.swift
+++ Catchy/Catchy/Modules/Groups/ViewControllers/QRCodeInviteController.swift
@@ -6,58 +6,115 @@
 //  Created by 임소은 on 1/18/25.
 //
 
+
 import SwiftUI
 import CoreImage.CIFilterBuiltins
 
-/// QR 코드 초대 로직을 처리하는 컨트롤러
 class QRCodeInviteController: ObservableObject {
-    // Published Properties for UI Binding
-    @Published var inviteCode: String = ""
-    @Published var userNickname: String = "초대 보내는 사람 닉네임"
+    @Published var inviteCode: String = "" // 초대 URL
+    @Published var userNickname: String = "초대 보내는 사람 닉네임" // 초대 보내는 사용자의 닉네임
     @Published var isLoading: Bool = true
+    @Published var qrCodeImage: UIImage? // QR 코드 이미지
 
     private let filter = CIFilter.qrCodeGenerator()
 
     // MARK: - Methods
-    
-    /// API연동 시뮬레이션
-    /// 초대 코드를 설정
+
+    /// API 연동 시뮬레이션
     func setupInviteCode() {
-        // TODO: - 실제 API 연동을 추가하여 그룹 ID를 생성하고 초대 URL을 설정
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            // TODO: - 실제 API 연동을 추가하여 그룹 ID를 생성하고 초대 URL을 설정
+            self.inviteCode = "https://developer.apple.com/documentation/coreimage/ciqrcodefeature" // 초대 URL 설정
+            self.qrCodeImage = self.generateQRCodeWithRepresentImage(from: self.inviteCode) // QR 코드 생성
+            self.userNickname = "김캐치" // 초대 보내는 사용자 닉네임 설정
+            self.isLoading = false
+        }
     }
 
-    
-    /// QR 코드 이미지 생성
-    func generateQRCode() -> UIImage? {
+    /// QR 코드 생성 함수
+    private func generateQRCode(from string: String) -> UIImage? {
+        guard !string.isEmpty else { return nil }
+
         let context = CIContext()
-        filter.message = Data(inviteCode.utf8)
-        if let outputImage = filter.outputImage,
-           let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-            return UIImage(cgImage: cgImage)
-        }
-        return nil
+        filter.message = Data(string.utf8)
+
+        guard let outputImage = filter.outputImage else { return nil }
+        let scaledQRImage = outputImage.transformed(by: CGAffineTransform(scaleX: 10, y: 10))
+
+        guard let cgImage = context.createCGImage(scaledQRImage, from: scaledQRImage.extent) else { return nil }
+        return UIImage(cgImage: cgImage)
     }
-    
+
+    /// QR 코드 생성 시 그룹대표이미지 삽입
+    private func generateQRCodeWithRepresentImage(from string: String) -> UIImage? {
+        guard let qrImage = generateQRCode(from: string),
+              let representImage = UIImage(named: "groupImage") else { return generateQRCode(from: string) }
+
+        return embedRepresentImageInQRCode(qrImage: qrImage, representImageRect: representImage)
+    }
+
+    private func embedRepresentImageInQRCode(qrImage: UIImage, representImageRect: UIImage) -> UIImage? {
+        let size = qrImage.size
+
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+
+        // QR 코드 그리기
+        qrImage.draw(in: CGRect(origin: .zero, size: size))
+
+        //그룹 대표 이미지
+        let representImageSize = CGSize(width: size.width * 0.2, height: size.height * 0.2)
+        let margin: CGFloat = size.width * 0.05
+        let backgroundSize = CGSize(width: representImageSize.width + margin, height: representImageSize.height + margin)
+
+        // 대표 이미지와 QR코드 사이에 거리 두기
+        let backgroundOrigin = CGPoint(
+            x: (size.width - backgroundSize.width) / 2,
+            y: (size.height - backgroundSize.height) / 2
+        )
+        let backgroundRect = CGRect(origin: backgroundOrigin, size: backgroundSize)
+
+        UIColor.white.setFill()
+        UIBezierPath(ovalIn: backgroundRect).fill()
+
+        // 그룹대표이미지 원형으로 클립
+        let representImageOrigin = CGPoint(
+            x: backgroundOrigin.x + margin / 2,
+            y: (backgroundOrigin.y + margin / 2)
+        )
+        let representImageFrame = CGRect(origin: representImageOrigin, size: representImageSize)
+
+        let path = UIBezierPath(ovalIn: representImageFrame)
+        path.addClip()
+        representImageRect.draw(in: representImageFrame)
+
+        let combinedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return combinedImage
+    }
+
     /// QR 코드 공유
     func shareQRCode() {
-        guard let qrImage = generateQRCode() else { return }
+        guard let qrImage = qrCodeImage else { return }
         let activityItems: [Any] = [qrImage]
         let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-        
+
         if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
            let rootViewController = scene.windows.first?.rootViewController {
             rootViewController.present(activityViewController, animated: true)
         }
     }
-    
+
     /// QR 코드 저장
     func saveQRCode() {
-        guard let qrImage = generateQRCode() else { return }
+        guard let qrImage = qrCodeImage else { return }
         UIImageWriteToSavedPhotosAlbum(qrImage, nil, nil, nil)
     }
-    
+
     /// 페이지 닫기
     func closePage() {
-        // 페이지를 닫는 동작 구현
+
+        // TODO: - 페이지 닫기 동작 구현
     }
 }
+
--- Catchy/Catchy/Modules/Groups/Views/QRCodeInviteView.swift
+++ Catchy/Catchy/Modules/Groups/Views/QRCodeInviteView.swift
@@ -1,56 +1,36 @@
-//
-//  Untitled.swift
-//  Catchy
-//
-//  Created by 임소은 on 1/18/25.
-//
-
 import SwiftUI
 
-/// QR 코드 초대 뷰
 struct QRCodeInviteView: View {
-    // Controller에서 전달된 데이터를 상태로 관리
     @ObservedObject var controller: QRCodeInviteController
 
     var body: some View {
         VStack(spacing: 0) {
-            // 닫기 버튼
             topCloseButton()
-            
+
             Spacer()
-            
-            // 사용자 닉네임 및 로딩 상태
+
             if controller.isLoading {
                 ProgressView("QR 코드를 생성 중입니다")
                     .padding()
+            } else if let qrImage = controller.qrCodeImage {
+                qrCodeContent(qrImage: qrImage)
             } else {
-                Text(controller.userNickname)
-                    .font(.Subtitle3)
-                    .foregroundStyle(Color.black)
-                    .padding(.bottom, 10)
+                Text("QR 코드를 생성할 수 없습니다.")
+                    .font(.body3)
             }
-            
-            // QR 코드 이미지
-            if !controller.isLoading, !controller.inviteCode.isEmpty {
-                qrCodeImage()
-            }
-            
-            // 공유 및 저장 버튼
+
             if !controller.isLoading {
                 bottomButtons()
             }
-            
+
             Spacer()
         }
         .onAppear(perform: controller.setupInviteCode) // 화면이 나타날 때 초대 코드 설정
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.white.ignoresSafeArea())
     }
-    
-    
-    // MARK: - Private Views
-    
-    //닫기버튼
+
+    // MARK: - 닫기 버튼
     private func topCloseButton() -> some View {
         HStack {
             Spacer()
@@ -60,38 +40,34 @@ struct QRCodeInviteView: View {
                 Image(systemName: "xmark")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 24, height: 24)
-                    .foregroundStyle(Color.gray)
-                    .padding()
-                    
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(Color.g4)
             }
             .frame(width: 24, height: 24)
             .contentShape(Rectangle())
         }
         .padding(.top, 20)
-        .padding(.trailing, 29)
+        .padding(.horizontal, 29)
     }
-    
-    //QR코드 이미지
-    private func qrCodeImage() -> some View {
-        if let qrImage = controller.generateQRCode() {
-            return AnyView(
-                Image(uiImage: qrImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 290)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .padding()
-            )
-        } else {
-            return AnyView(
-                Text("QR 코드를 생성할 수 없습니다.")
-                    .foregroundStyle(Color.red)
-            )
+
+    // MARK: - QR 코드 및 닉네임
+    private func qrCodeContent(qrImage: UIImage) -> some View {
+        VStack(spacing: 10) {
+            // 초대하는 사용자의 닉네임 텍스트
+            Text(controller.userNickname)
+                .font(.Subtitle3)
+                .foregroundStyle(Color.g7)
+
+            // QR 코드 이미지
+            Image(uiImage: qrImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
         }
     }
-    
-    //외부공유, QR다운 버튼
+
+    // MARK: - 외부공유, QR다운 버튼
     private func bottomButtons() -> some View {
         HStack(spacing: 15) {
             Button(action: {
@@ -103,10 +79,10 @@ struct QRCodeInviteView: View {
                         .frame(width: 42, height: 42)
                     Text("외부공유")
                         .font(.body3)
-                        .foregroundStyle(Color.gray)
+                        .foregroundStyle(Color.g5)
                 }
             }
-            
+
             Button(action: {
                 controller.saveQRCode()
             }) {
@@ -116,7 +92,7 @@ struct QRCodeInviteView: View {
                         .frame(width: 42, height: 42)
                     Text("QR저장")
                         .font(.body3)
-                        .foregroundStyle(Color.gray)
+                        .foregroundStyle(Color.g5)
                 }
             }
         }
@@ -125,17 +101,14 @@ struct QRCodeInviteView: View {
 }
 
 // MARK: - Preview
-
 struct QRCodeInviteView_Previews: PreviewProvider {
     static var previews: some View {
         let controller = QRCodeInviteController()
-        controller.isLoading = false // 로딩 완료 상태로 설정
-        controller.inviteCode = "https://developer.apple.com/documentation/coreimage/" // 미리 보기 초대 코드
-        controller.userNickname = "초대 보내는 사람 닉네임" // 미리 보기 닉네임
-        
+        controller.isLoading = false
+        controller.inviteCode = "https://developer.apple.com/documentation/coreimage/ciqrcodefeature"
+        controller.userNickname = "김캐치"
+
         return QRCodeInviteView(controller: controller)
-            
     }
 }
-
 
