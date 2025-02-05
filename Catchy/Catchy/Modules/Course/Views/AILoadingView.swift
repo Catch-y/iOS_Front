60e587a63bb6861a893da28dc4999579c95b2116
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2025-02-05 19:05:04 +0900

    ✨ AI 코스 생성하기 API 연결

--- Catchy/Catchy/Modules/Course/Views/AILoadingView.swift
+++ Catchy/Catchy/Modules/Course/Views/AILoadingView.swift
@@ -10,35 +10,184 @@ import SwiftUI
 /// AI 생성 버튼 탭 시 나타나는 뷰
 struct AILoadingView: View {
     
+    @StateObject var viewModel: AICourseCreateViewModel
+        
+    init(container: DIContainer) {
+        self._viewModel = StateObject(wrappedValue: .init(container: container))
+    }
     
     var body: some View {
         
-        VStack(spacing: 88) {
-            infoText
+        GeometryReader { geometry in
+            
+            /// 화면의 가로 크기
+            let width = geometry.size.width
         
-            ZStack {
-                Icon.loading.image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+            VStack(spacing: 120) {
+                infoText
+                
+                ZStack(alignment: .top) {
+                    
+                    gradient
+                                
+                    loadingView(with: width)
+                    
+                    
+                }.onAppear {
+                    withAnimation(.easeIn(duration: viewModel.duration).delay(0)) {
+                        viewModel.showRedPin = true
+                    }
+                    withAnimation(.easeIn(duration: viewModel.duration).delay(viewModel.duration)) {
+                        viewModel.showYellowPin = true
+                    }
+                    withAnimation(.easeIn(duration: viewModel.duration).delay(viewModel.duration * 2)) {
+                        viewModel.showPurplePin = true
+                    }
+                    withAnimation(.easeIn(duration: viewModel.duration).delay(viewModel.duration * 3)) {
+                        viewModel.showBluePin = true
+                    }
+                    
+                    withAnimation(
+                        Animation
+                            .easeInOut(duration: viewModel.duration)
+                            .repeatForever(autoreverses: true)
+                            .delay(0)
+                    ) { viewModel.floatingRedPin = true}
+                    withAnimation(
+                        Animation
+                            .easeInOut(duration: viewModel.duration)
+                            .repeatForever(autoreverses: true)
+                            .delay(viewModel.duration)
+                    ) { viewModel.floatingYellowPin = true }
+                    withAnimation(
+                        Animation
+                            .easeInOut(duration: viewModel.duration)
+                            .repeatForever(autoreverses: true)
+                            .delay(viewModel.duration * 2)
+                    ) { viewModel.floatingPurplePin = true }
+                    withAnimation(
+                        Animation
+                            .easeInOut(duration: viewModel.duration)
+                            .repeatForever(autoreverses: true)
+                            .delay(viewModel.duration * 3)
+                    ) { viewModel.floatingBluePin = true }
+                    
+                    // TODO: - 다음 화면으로 넘어가는 로직 구현.
+                    DispatchQueue.main.async {
+                        if !viewModel.isLoading {
+                            
+                        }
+                    }
+                    
+                }
+                
             }
-            
-            
+        
         }
-        .ignoresSafeArea(edges: .top)
     }
     
-    
+    /// 안내 문구
     private var infoText: some View {
         VStack(spacing: 14) {
-            Text("\(DataFormatter.shared.makeStyledText(for: UserState.shared.getUserNickname(), with: .Subtitle1)) 님에게 \(DataFormatter.shared.makeStyledText(for: "딱 맞는 코스"))를\n생성중이에요!")
-                .font(.Subtitle1)
-                .foregroundStyle(.g7)
-                .multilineTextAlignment(.center)
-                .lineSpacing(3.3)
+            Text(
+                "\(DataFormatter.shared.makeStyledText(for: UserState.shared.getUserNickname(), with: .Subtitle1)) 님에게 \(DataFormatter.shared.makeStyledText(for: "딱 맞는 코스"))를\n생성중이에요!"
+            )
+            .font(.Subtitle1)
+            .foregroundStyle(.g7)
+            .multilineTextAlignment(.center)
+            .lineSpacing(3.3)
+            
             Text("AI가 열심히 만들고 있어요! 조금만 기다려 주세요.")
                 .font(.body2)
                 .foregroundStyle(.g4)
         }
+        .padding(.top, 73)
+    }
+    
+    /// 로딩 뷰
+    private func loadingView(with width: CGFloat) -> some View {
+        Icon.loading.image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .overlay {
+                
+                /// 빨강색 핀
+                pinImage(
+                    Icon.red_pin.image,
+                    screenWidth: width,
+                    xOffset: width * 0.3,
+                    yOffset: -width * 0.45,
+                    isShowing: viewModel.showRedPin,
+                    isFloating: viewModel.floatingRedPin
+                )
+                
+                /// 노랑색 핀
+                pinImage(
+                    Icon.yellow_pin.image,
+                    screenWidth: width,
+                    xOffset: -width * 0.18,
+                    yOffset: -width * 0.125,
+                    isShowing: viewModel.showYellowPin,
+                    isFloating: viewModel.floatingYellowPin
+
+                )
+                
+                /// 보라색 핀
+                pinImage(
+                    Icon.purple_pin.image,
+                    screenWidth: width,
+                    xOffset: width * 0.21,
+                    yOffset: width * 0.37,
+                    isShowing: viewModel.showPurplePin,
+                    isFloating: viewModel.floatingPurplePin
+
+                )
+                
+                /// 파랑색 핀
+                pinImage(
+                    Icon.blue_pin.image,
+                    screenWidth: width,
+                    xOffset: -width * 0.35,
+                    yOffset: width * 0.315,
+                    isShowing: viewModel.showBluePin,
+                    isFloating: viewModel.floatingBluePin
+                )
+
+            }
+    }
+    
+    
+    /// 핀 이미지를 만듭니다.
+    /// - Parameters:
+    ///   - image: 핀 이미지
+    ///   - screenWidth: 화면 전체의 가로 크기 (핀 이미지의 위치 설정을 위함)
+    ///   - xOffset: x 오프셋
+    ///   - yOffset: y 오프셋
+    ///   - isShowing: 핀이 보이고 있는가?
+    /// - Returns: 이미지 객체를 반환
+    private func pinImage(_ image: Image, screenWidth: CGFloat, xOffset: CGFloat, yOffset: CGFloat, isShowing: Bool, isFloating: Bool) -> some View {
+        
+        image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: screenWidth * 0.07)
+            .offset(
+                x: xOffset,
+                y: yOffset + (isFloating ? -8 : 0)
+            )
+            .opacity(isShowing ? 1 : 0)
+    }
+    
+    /// 그라데이션
+    private var gradient: some View {
+        
+        /// #FF517D
+        let startColor = Color(red:255/255, green: 81/255, blue: 125/255).opacity(0.1)
+        
+        return LinearGradient(gradient: Gradient(colors: [.white, startColor, .white]),
+                       startPoint: .top, endPoint: .bottom)
+        .frame(maxWidth: .infinity)
+        .padding(.bottom, 200)
     }
     
 }
@@ -49,7 +198,7 @@ struct AILoadingView_Previews: PreviewProvider {
             ["iPhone 16 Pro Max", "iPhone 11", "iPhone 12 mini"],
             id: \.self
         ) { deviceName in
-            AILoadingView()
+            AILoadingView(container: DIContainer())
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
         }
