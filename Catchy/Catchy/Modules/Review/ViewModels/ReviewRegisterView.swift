ffb5d2b61d4363780d6fc3649d9dab553223458c
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2025-02-07 16:31:34 +0900

    ✨ 뷰 모델 API 메소드 구현

--- Catchy/Catchy/Modules/Review/ViewModels/ReviewRegisterView.swift
+++ Catchy/Catchy/Modules/Review/ViewModels/ReviewRegisterView.swift
@@ -26,9 +26,18 @@ class ReviewRegisterViewModel: ObservableObject {
     /// 리뷰 방문 날짜
     @Published var visitedDate: String?
     
+    /// 리뷰 방문 날짜 리스트
+    @Published var visitedDateListResponse: PlaceVisitedDateResponse?
+    
+    /// 리뷰 등록 응답
+    @Published var reviewSubmissionResponse: PlaceReviewSubmissionResponse?
+    
+    /// 리뷰 등록 완료 되었는가
+    @Published var isRegiestered: Bool = false
     
     // TODO: - 이미지 post 처리
     @Published var images: [String]?
+    
     
     
     init(container: DIContainer) {
@@ -37,4 +46,86 @@ class ReviewRegisterViewModel: ObservableObject {
     
     
     
+}
+
+extension ReviewRegisterViewModel {
+    
+    // MARK: - API 요청이 있는 메소드
+    /// 장소 방문 날짜 리스트 조회 API
+    func getPlaceVisitedDateList(placeId: Int) {
+        
+        container.useCaseProvider.placeUseCase
+            .executeGetPlaceVisitedDates(placeId: placeId)
+            .tryMap{ responseData -> ResponseData<PlaceVisitedDateResponse> in
+                if !responseData.isSuccess{
+                    throw APIError
+                        .serverError(
+                            message: responseData.message,
+                            code: responseData.code
+                        )
+                }
+                
+                return responseData
+            }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: {
+                [weak self] completion in
+                guard let self = self else { return }
+                    
+                switch completion {
+                case .finished:
+                    print("✅ Get PlaceVisitedDateList Server Completed")
+                case .failure(let failure):
+                    print("❌ Get PlaceVisitedDateList Failed: \(failure)")
+                }
+            },receiveValue: { [weak self] response in
+                guard let self = self else { return }
+                
+                if let response = response.result{
+                    self.visitedDateListResponse = response
+                }
+                
+            })
+            .store(in: &cancellables)
+    }
+    
+    /// 장소 평점/리뷰 달기 API
+    func postPlaceReviewSubmission(request: PlaceReviewSubmissionRequest){
+        
+        container.useCaseProvider.placeUseCase
+            .executePostPlaceReviewSubmission(request: request)
+            .tryMap{ responseData -> ResponseData<PlaceReviewSubmissionResponse> in
+                if !responseData.isSuccess{
+                    throw APIError
+                        .serverError(
+                            message: responseData.message,
+                            code: responseData.code
+                        )
+                }
+                
+                return responseData
+            }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: {
+                [weak self] completion in
+                guard let self = self else { return }
+                    
+                self.isRegiestered = true
+                
+                switch completion {
+                case .finished:
+                    print("✅ Get PlaceVisitedDateList Server Completed")
+                case .failure(let failure):
+                    print("❌ Get PlaceVisitedDateList Failed: \(failure)")
+                }
+            },receiveValue: { [weak self] response in
+                guard let self = self else { return }
+                
+                if let response = response.result{
+                    self.reviewSubmissionResponse = response
+                }
+                
+            })
+            .store(in: &cancellables)
+    }
 }
