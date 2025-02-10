e74d1f3db82d45ed9492a94a3e80ca2a8ea442b3
JEONG-J <ikejhc159@gmail.com>
2025-02-10 17:18:54 +0900

    ✨ 디바이스 토큰 조회
    디바이스 토큰 조회 AppDelegate 설계

---
+++ Catchy/Catchy/APP/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  Catchy
+//
+//  Created by 정의찬 on 2/10/25.
+//
+
+import UIKit
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        requestNotificationPermission()
+        return true
+    }
+    
+    private func requestNotificationPermission() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenStriing = deviceToken.map { String(format: "%02x", $0)}.joined()
+        print("디바이스 토큰: \(tokenStriing)")
+        NotificationCenter.default.post(name: .deviceTokenReceived, object: tokenStriing)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: any Error) {
+        print("Faield to register: \(error.localizedDescription)")
+    }
+}
+
+extension Notification.Name {
+    static let deviceTokenReceived = Notification.Name("deviceTokenReceived")
+}
