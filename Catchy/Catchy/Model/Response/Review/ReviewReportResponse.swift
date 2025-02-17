71970b0d1b613b07bef05a3ae6e5069f166ef19b
yongbin <chosh0312@naver.com>
2025-02-17 18:20:23 +0900

    💄 리뷰 신고하기 버튼 수정
    신고하기 버튼 수정

--- Catchy/Catchy/Model/Response/Review/ReviewReportResponse.swift
+++ Catchy/Catchy/Model/Response/Review/ReviewReportResponse.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct ReviewReportResponse: Codable {
-    let reviewId: Int            // 리뷰 ID
-    let reviewType: ReviewType     // 리뷰 타입 선택
-    let message: String          // 신고 성공 메시지
+    let reportId: Int            /* 신고 ID */
+    let reviewType: ReviewType     /* 리뷰 타입 선택 */
+    let message: String          /* 신고 성공 메시지 */
 }
