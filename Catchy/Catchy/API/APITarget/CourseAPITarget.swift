5ff2b46aa6af5e2488dd7b6d3d25f69a2ffa68d7
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2025-02-18 14:43:16 +0900

    ✨ Mimetype 수정

--- Catchy/Catchy/API/APITarget/CourseAPITarget.swift
+++ Catchy/Catchy/API/APITarget/CourseAPITarget.swift
@@ -702,7 +702,7 @@ extension CourseAPITarget {
          
         for (_, image) in courseImage.enumerated() {
             if let image = image.jpegData(compressionQuality: 0.8) {
-                let multipartData = MultipartFormData(provider: .data(image), name: "courseImage", fileName: "courseImage.jpg", mimeType: "images/jpeg")
+                let multipartData = MultipartFormData(provider: .data(image), name: "courseImage", fileName: "courseImage.jpg", mimeType: "/")
                 formData.append(multipartData)
             
             }
@@ -742,7 +742,7 @@ extension CourseAPITarget {
         }
                 
         if let image = courseImage.jpegData(compressionQuality: 0.8) {
-            let multipartData = MultipartFormData(provider: .data(image), name: "courseImage", fileName: "courseImage.jpg", mimeType: "images/jpeg")
+            let multipartData = MultipartFormData(provider: .data(image), name: "courseImage", fileName: "courseImage.jpg", mimeType: "/")
             formData.append(multipartData)
 
         }
