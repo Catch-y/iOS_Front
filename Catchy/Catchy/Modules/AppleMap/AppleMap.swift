60d2f8f59cee923380d3aba36c59949db0220d35
JEONG-J <ikejhc159@gmail.com>
2025-02-21 01:34:28 +0900

    🚑️ GPS Sensing
    GPS Sensing c추가

--- Catchy/Catchy/Modules/AppleMap/AppleMap.swift
+++ Catchy/Catchy/Modules/AppleMap/AppleMap.swift
@@ -29,6 +29,16 @@ struct AppleMap: UIViewRepresentable {
         mapView.camera = camera
         mapView.showsCompass = false
         
+        mapView.userTrackingMode = .followWithHeading
+        
+        let trackingButton = MKUserTrackingButton(mapView: mapView)
+        trackingButton.frame = CGRect(x: 20, y: 50, width: 40, height: 40)
+        trackingButton.layer.cornerRadius = 8
+        trackingButton.backgroundColor = UIColor.white.withAlphaComponent(0.7)
+        trackingButton.autoresizingMask = [.flexibleRightMargin, .flexibleBottomMargin]
+        
+        mapView.addSubview(trackingButton)
+        
         return mapView
     }
     
