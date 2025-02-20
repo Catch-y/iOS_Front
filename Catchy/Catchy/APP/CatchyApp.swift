b22f1bb7461acefa85dd1416ac6c6388c21d2d02
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2025-02-21 02:15:39 +0900

    💄 캐치 앱 진입점 수정

--- Catchy/Catchy/APP/CatchyApp.swift
+++ Catchy/Catchy/APP/CatchyApp.swift
@@ -21,23 +21,20 @@ struct CatchyApp: App {
     
     var body: some Scene {
         WindowGroup {
-            CatchyTabView()
-                .environmentObject(container)
-                .environmentObject(appFlowViewModel)
-//            switch appFlowViewModel.appState {
-//            case .onBoarding:
-//                OnboardingView(viewModel: appFlowViewModel)
-//            case .login:
-//                LoginView(container: container, appFlowViewModel: appFlowViewModel)
-//                    .environmentObject(container)
-//                    .environmentObject(appFlowViewModel)
-//            case .preferrenceSurvey:
-//                PreferencePageView(container: container, appFlowViewModel: appFlowViewModel)
-//            case .tabView:
-//                CatchyTabView()
-//                    .environmentObject(container)
-//                    .environmentObject(appFlowViewModel)
-//            }
+            switch appFlowViewModel.appState {
+            case .onBoarding:
+                OnboardingView(viewModel: appFlowViewModel)
+            case .login:
+                LoginView(container: container, appFlowViewModel: appFlowViewModel)
+                    .environmentObject(container)
+                    .environmentObject(appFlowViewModel)
+            case .preferrenceSurvey:
+                PreferencePageView(container: container, appFlowViewModel: appFlowViewModel)
+            case .tabView:
+                CatchyTabView()
+                    .environmentObject(container)
+                    .environmentObject(appFlowViewModel)
+            }
         }
     }
 }
