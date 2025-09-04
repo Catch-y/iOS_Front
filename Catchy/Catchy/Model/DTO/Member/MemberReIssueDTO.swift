a3d2ad34e021ec5885033b0eafd6227efd3f58e4
AppleDeveloper <ikejhc159@gmail.com>
2025-09-04 20:45:04 +0900

    ♻️ [Refac] 모델 구조 리팩토링
    - Common, Member, User 관련 모델 위치 및 내용 수정

---
+++ Catchy/Catchy/Model/DTO/Member/MemberReIssueDTO.swift
@@ -0,0 +1,13 @@
+//
+//  MemberReIssueDTO.swift
+//  Catchy
+//
+//  Created by Apple Coding machine on 9/4/25.
+//
+
+import Foundation
+
+struct MemberReIssueResponse: Codable {
+    var accessToken: String
+    var refreshToken: String
+}
